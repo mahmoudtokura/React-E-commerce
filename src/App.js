1dc759f17f5e8754a8dce7efe72d3683341f66be
Mahmoud Tokura <mahmoudbintokura@gmail.com>
2020-08-20 11:38:24 +0100

    added shop page, collectection preview component

--- src/App.js
+++ src/App.js
@@ -2,16 +2,7 @@ import React from "react";
 import { Switch, Route  } from "react-router-dom";
 import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
-
-
-const Hats = () => {
-  return (
-    <div>
-      <h1>Hats page</h1>
-    </div>
-  )
-}
-
+import ShopPage from "./pages/shop/shop.component";
 
 
 function App() {
@@ -19,7 +10,7 @@ function App() {
     <div>
       <Switch>
         <Route exact path='/' component={HomePage}/>
-        <Route path='/hats' component={Hats}/>
+        <Route path='/shop' component={ShopPage}/>
       </Switch>
     </div>
   );
