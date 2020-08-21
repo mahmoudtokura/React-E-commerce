5601296becbde14bcef0d3d3b2d2161711345bbf
Mahmoud Tokura <mahmoudbintokura@gmail.com>
2020-08-21 08:28:51 +0100

    Implemented auth with email and password

--- src/App.js
+++ src/App.js
@@ -1,46 +1,57 @@
 import React from "react";
-import { Switch, Route  } from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
 import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 import Header from "./components/header/header.component";
 import signInAndSignUpPage from "./pages/sign-in-and-sign-up.component/sign-in-and-sign-up.component";
-import { auth } from './firebase/firebase.utils'
-
+import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
 
 class App extends React.Component {
-  constructor(props){
-    super(props)
+  constructor(props) {
+    super(props);
 
     this.state = {
-      currentUser: null
-    }
+      currentUser: null,
+    };
   }
 
-  unsubscribFromAuth = null
+  unsubscribFromAuth = null;
 
-  componentDidMount(){
-    this.unsubscribFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({currentUser: user})
-      console.log({user});
-    })
+  componentDidMount() {
+    this.unsubscribFromAuth = auth.onAuthStateChanged(async (userAuth) => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+
+        userRef.onSnapshot((snapshot) => {
+          this.setState({
+            currentUser: {
+              id: snapshot.id,
+              ...snapshot.data(),
+            },
+          });
+          console.log(this.state);
+        });
+      }
+      this.setState({ currentUser: userAuth });
+    });
   }
 
   componentWillUnmount() {
-    this.unsubscribFromAuth()
+    this.unsubscribFromAuth();
   }
-  
+
   render() {
     return (
       <div>
-        <Header currentUser={this.state.currentUser}/>
+        <Header currentUser={this.state.currentUser} />
         <Switch>
-          <Route exact path='/' component={HomePage}/>
-          <Route path='/shop' component={ShopPage}/>
-          <Route path='/signin' component={signInAndSignUpPage}/>
+          <Route exact path="/" component={HomePage} />
+          <Route path="/shop" component={ShopPage} />
+          <Route path="/signin" component={signInAndSignUpPage} />
         </Switch>
       </div>
-    )
+    );
   }
 }
 
